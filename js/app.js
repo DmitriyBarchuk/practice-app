65a2e4e581f9294e1bae24f15e32e5073a16ee2c
DmitriyBarchuk <developmentiteasy@gmail.com>
2017-02-19 15:59:06 +0200

    Created primitive containers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
+var Comments = React.createClass({
+  render: function() {
+    return (
+      <div className="comments">
+        Нет новостей - комментировать нечего.
+      </div>
+    );
+  }
+});
+
+var News = React.createClass({
+  render: function() {
+    return (
+      <div className="news">
+        Я компонент News и иногда я могу отображать новости, если они есть.
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Что то выводим! Что то рендерим.
+        <News />
+        <Comments />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
