c02dcb5ce913ceae8bd4595e873e6cdeff75381f
DmitriyBarchuk <developmentiteasy@gmail.com>
2017-02-19 18:40:49 +0200

    Used Props. Create a template

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+var viewd_news = [
+  {
+    author: 'Dmitriy Barchuk',
+    preview_text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Alex Barchuk',
+    preview_text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Ghost',
+    preview_text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
 var Comments = React.createClass({
   render: function() {
     return (
@@ -10,9 +25,19 @@ var Comments = React.createClass({
 
 var News = React.createClass({
   render: function() {
+    var last_news = this.props.last_news;
+    var newsShablon = last_news.map(function(item, id){
+        return (
+          <div key={id}>
+            <p className="news_author">{item.author}:</p>
+            <p className="news_preview_text">{item.preview_text}</p>
+          </div>
+        )
+    })
+
     return (
       <div className="news">
-        Я компонент News и иногда я могу отображать новости, если они есть.
+      {newsShablon}
       </div>
     );
   }
@@ -23,7 +48,7 @@ var App = React.createClass({
     return (
       <div className="app">
         Что то выводим! Что то рендерим.
-        <News />
+        <News last_news={viewd_news} /> {/*коммент*/}
         <Comments />
       </div>
     );
